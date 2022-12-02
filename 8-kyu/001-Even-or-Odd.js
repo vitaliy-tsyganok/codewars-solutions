f24bb4f691a04294f7f459c5e4a4e82e43cdb2da
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-12-02 15:33:22 +0400

    +5 задач (8 уровень)

---
+++ 8-kyu/001-Even-or-Odd.js
@@ -0,0 +1,7 @@
+/* https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+function evenOrOdd(number) {
+	return number % 2 ? 'Odd' : 'Even'
+}
---
+++ 8-kyu/003-Convert-a-Number-to-a-String.js
@@ -0,0 +1,16 @@
+/* https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100" */
+
+function numberToString(num) {
+	return num.toString()
+	// return String(num)
+	// return `${num}`
+}
