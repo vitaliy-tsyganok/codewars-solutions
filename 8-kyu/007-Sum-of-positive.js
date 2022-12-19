225e1a78f377cbc1d3221bd6c56fc67fea91205c
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-12-19 09:41:11 +0300

    Edited 007-Sum-of-positive

--- 8-kyu/007-Sum-of-positive.js
+++ 8-kyu/007-Sum-of-positive.js
@@ -7,19 +7,5 @@ Example [1,-4,7,12] => 1 + 7 + 12 = 20
 Note: if there is nothing to sum, the sum is default to 0. */
 
 function positiveSum1(arr) {
-	return arr.reduce((sum, num) => (num > 0 ? sum + num : sum), 0)
-}
-
-function positiveSum2(arr) {
-	return arr.reduce((a, b) => a + (b > 0 ? b : 0), 0)
-}
-
-function positiveSum3(arr) {
-	var total = 0
-	for (i = 0; i < arr.length; i++) {
-		if (arr[i] > 0) {
-			total += arr[i]
-		}
-	}
-	return total
+	return arr.reduce((sum, num) => (num > 0 ? sum + num : sum), 0);
 }
