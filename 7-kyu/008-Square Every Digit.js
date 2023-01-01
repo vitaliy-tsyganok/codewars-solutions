c19b9690ede10e597e740d3f218b4b7eb24a7cbf
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2023-01-01 17:19:24 +0300

    7 уровень (+3 задачи)

---
+++ 7-kyu/008-Square Every Digit.js
@@ -0,0 +1,22 @@
+/* https://www.codewars.com/kata/546e2562b03326a88e000020
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer */
+
+// Мое решение
+function squareDigits(num) {
+	return +String(num)
+		.split('')
+		.reduce((acc, val) => acc + val ** 2, '');
+}
+
+// Альтернативное решение через map
+function squareDigits(num) {
+	return +String(num)
+		.split('')
+		.map((el) => el ** 2)
+		.join('');
+}
